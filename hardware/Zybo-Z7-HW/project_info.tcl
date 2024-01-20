12ddc89a93ad87911018cbf92adec637cd421bc8
Paran Lee <p4ranlee@gmail.com>
2024-01-20 11:03:47 +0900

    Add comment set_property "board_part" issue on project_info.tcl

--- hardware/Zybo-Z7-HW/project_info.tcl
+++ hardware/Zybo-Z7-HW/project_info.tcl
@@ -2,7 +2,8 @@
 proc set_project_properties_post_create_project {proj_name} {
     set project_obj [get_projects $proj_name]
     set_property "part" "xc7z020clg400-1" $project_obj
-    #set_property "board_part" "digilentinc.com:zybo-z7-20:part0:1.1" $project_obj
+    # See https://support.xilinx.com/s/question/0D52E00006hpLo5SAE/board-4967-the-boardpart-definition-was-not-found?language=en_US
+    # set_property "board_part" "digilentinc.com:zybo-z7-20:part0:1.1" $project_obj
     set_property "default_lib" "xil_defaultlib" $project_obj
     set_property "simulator_language" "Mixed" $project_obj
     set_property "target_language" "VHDL" $project_obj
